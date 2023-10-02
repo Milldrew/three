26cf8904f3cf84d90ffe46db65fd0ec77958a847
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2023-10-02 16:54:42 -0600

    📅  | Mon Oct  2 16:54:42 MDT 2023

--- ng-workspace/src/app/app.component.ts
+++ ng-workspace/src/app/app.component.ts
@@ -1,10 +1,13 @@
+import * as THREE from 'three';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-workspace';
+  constructor() {
+    console.log(THREE);
+  }
 }
